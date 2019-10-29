a082491d4190e7372c23dcc0a871c95ceab3f65f
melodist <melt.ep00@gmail.com>
2019-10-29 16:06:57 +0900

    Added analysis for each patient

--- Evaluate.py
+++ Evaluate.py
@@ -25,41 +25,57 @@ trained_model.load_weights(path_model)
 # Load Images
 ind_CT = [[230, 380], [150, 370]]
 ind_PT = [[230, 380], [150, 370]]
-path = './Example'
+path = './Examples'
 
-img_CT, img_PT = PatchExtraction.stackImages(path, ind_CT, ind_PT)
-patches_CT = PatchExtraction.patch_extraction(img_CT)
-patches_PT = PatchExtraction.patch_extraction(img_PT)
-
-# Extract Features
-print(f"Extract Features...")
-features = trained_model.predict([patches_CT, patches_PT], steps=1)
-
-# Using K-means
-print(f"K-Means Clustering...")
-num_labels = 10
-model_k_means = KMeans(n_clusters=num_labels)
-model_k_means.fit(features)
-
-# Merging Patches
-num_x = 44
-num_y = 30
-stride = 5
-
+# Make Results Folder
 now = datetime.now()
-path_files = f"./Results_{now.strftime('%Y%m%d_%H%M%S')}/"
-os.makedirs(path_files)
-label_predict = model_k_means.fit_predict(features)
-label_predict_batch = label_predict.reshape((-1, num_y * num_x))
+path_result = f"./Results_{now.strftime('%Y%m%d_%H%M%S')}/"
+os.makedirs(path_result)
 
-for i in range(label_predict_batch.shape[0]):
-    filename = str(i) + '.png'
-    mask = ImageProcessing.merging_patches(label_predict_batch[i, :], num_labels, num_y, num_x, stride)
-    for j in range(num_labels):
-        ImageProcessing.save_image(mask[:, :, j], f'Results_{j}_' + filename, path_files)
-    # save original image as reference
-    cv2.imwrite(path_files + 'CT' + filename, img_CT[i, :, :, 0]*255)
-    cv2.imwrite(path_files + 'PT' + filename, img_PT[i, :, :, 0]*255)
+# Print Patients Number
+patient_dir = os.listdir(path)
+print(f'Patients Number: {len(patient_dir)}')
 
-ImageProcessing.ImageBlending(path_files)
+for path_patient in patient_dir:
+    addr_patient = f'{path}/{path_patient}/'
+    path_files = path_result + path_patient + '/'
+    os.makedirs(path_files)
+
+    img_CT, img_PT = PatchExtraction.stackImages(addr_patient, ind_CT, ind_PT)
+    patches_CT = PatchExtraction.patch_extraction(img_CT)
+    patches_PT = PatchExtraction.patch_extraction(img_PT)
+
+    # Extract Features
+    print(f"Extract Features...")
+    features = trained_model.predict([patches_CT, patches_PT], steps=1)
+
+    # Using K-means
+    print(f"K-Means Clustering...")
+    num_labels = 10
+    model_k_means = KMeans(n_clusters=num_labels)
+    model_k_means.fit(features)
+
+    # Merging Patches
+    num_x = 44
+    num_y = 30
+    stride = 5
+
+    label_predict = model_k_means.fit_predict(features)
+    label_predict_batch = label_predict.reshape((-1, num_y * num_x))
+
+    # Extract File Names
+    for root, dirs, files in os.walk(os.path.join(path, path_patient)):
+        file_list = files
+        file_list.sort()
+
+    print(f'Merging Patches...')
+    for i, filename in enumerate(file_list):
+        mask = ImageProcessing.merging_patches(label_predict_batch[i, :], num_labels, num_y, num_x, stride)
+        for j in range(num_labels):
+            ImageProcessing.save_image(mask[:, :, j], f'Results_{j}_' + filename, path_files)
+        # save original image as reference
+        cv2.imwrite(path_files + 'CT_' + filename, img_CT[i, :, :, 0]*255)
+        cv2.imwrite(path_files + 'PT_' + filename, img_PT[i, :, :, 0]*255)
+
+    # ImageProcessing.ImageBlending(path_files)
 print(f"Done.")
