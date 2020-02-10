9bc02dd14214e996f9980f7d71cb98940472301e
MELT <melt.ep00@gmail.com>
2020-02-11 00:58:27 +0900

    Modified docstrings

--- Analysis/SampleVisualization.py
+++ Analysis/SampleVisualization.py
@@ -1,14 +1,14 @@
-# Sample Visualization
-# 1-1. Extract Features using initial network
-# 1-2. Extract Features using trained network
-# 2. Using K-means to classify the patches
-# 3. Dimension reduction using PCA
-# 4. Visualize results
-
+"""
+    Sample Visualization
+    Make 2-D image of sample distribution
+    1-1. Extract Features using initial network
+    1-2. Extract Features using trained network
+    2. Using K-means to classify the patches
+    3. Dimension reduction using PCA
+    4. Visualize results
+"""
 import tensorflow as tf
-import NetworkKeras
-import numpy as np
-import cv2
+from Network import NetworkKeras
 import os
 import time
 from Extraction import PatchExtraction
@@ -17,81 +17,93 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-tf.enable_eager_execution()
 
-time_start = time.time()
-path_model = './model/JULE/'
-# Extract Features using trained network
-# Load model
-input_shape = (17 * 17)
-embedding_size = 150
-trained_model = NetworkKeras.create_base_network(input_shape, embedding_size, 0.5)
-initial_model = NetworkKeras.create_base_network(input_shape, embedding_size, 0.5)
-trained_model.load_weights(path_model)
+def SampleVisualization(path_model, path_image):
+    """ Visualize sample distribution using PCA.
+        The result image will be saved on 'Results_%Y%m%d_%H%M%S'
 
-# Load Images
-ind_CT = [[230, 380], [150, 370]]
-ind_PT = [[230, 380], [150, 370]]
-path = '../Test'
+    Input
+    ______
+    path_model: path of trained model
+    path_image: path of test image
 
-# Make Results Folder
-now = datetime.now()
-path_result = f"./Results_{now.strftime('%Y%m%d_%H%M%S')}/"
-os.makedirs(path_result)
+    Output
+    ______
+    """
+    tf.enable_eager_execution()
 
-# Print Patients Number
-patient_dir = os.listdir(path)
-print(f'Patients Number: {len(patient_dir)}')
-
-for path_patient in patient_dir:
-    addr_patient = f'{path}/{path_patient}/'
-
-    img_CT, img_PT = PatchExtraction.stackImages(addr_patient, ind_CT, ind_PT)
-    patches_CT, patches_PT = PatchExtraction.patch_extraction_thres(img_CT, img_PT, 0)
-
-    # Extract Features using initial network
-    print(f"Extract Features using initial network...")
-    features_init = initial_model.predict([patches_CT, patches_PT], steps=1)
+    time_start = time.time()
 
     # Extract Features using trained network
-    print(f"Extract Features...")
-    features = trained_model.predict([patches_CT, patches_PT], steps=1)
+    # Load model
+    input_shape = (17 * 17)
+    embedding_size = 150
+    trained_model = NetworkKeras.create_base_network(input_shape, embedding_size, 0.5)
+    initial_model = NetworkKeras.create_base_network(input_shape, embedding_size, 0.5)
+    trained_model.load_weights(path_model)
 
-    # Using K-means
-    print(f"K-means Clustering...")
-    num_labels = 5
-    model_k_means = KMeans(n_clusters=num_labels, random_state=0)
-    model_k_means.fit(features)
-    label_predict = model_k_means.fit_predict(features)
+    # Load Images
+    ind_CT = [[230, 380], [150, 370]]
+    ind_PT = [[230, 380], [150, 370]]
 
-    # Dimension reduction using PCA
-    pca = PCA(n_components=2)
-    features_low = pca.fit_transform(features)
-    features_init_low = pca.transform(features_init)
+    # Make Results Folder
+    now = datetime.now()
+    path_result = f"./Results_{now.strftime('%Y%m%d_%H%M%S')}/"
+    os.makedirs(path_result)
 
-    colors = ['salmon', 'orange', 'steelblue', 'violet', 'khaki']
-    fig, ax = plt.subplots(2, figsize=(5, 5), constrained_layout=True)
+    # Print Patients Number
+    patient_dir = os.listdir(path_image)
+    print(f'Patients Number: {len(patient_dir)}')
 
-    for i in range(5):
-        data_init = features_init_low[label_predict == i]
-        X_init = data_init[:, 0]
-        Y_init = data_init[:, 1]
-        ax[0].scatter(X_init, Y_init, color=colors[i], label=i, s=1)
+    for path_patient in patient_dir:
+        addr_patient = f'{path_image}/{path_patient}/'
 
-        data = features_low[label_predict == i]
-        X = data[:, 0]
-        Y = data[:, 1]
-        ax[1].scatter(X, Y, color=colors[i], label=i, s=1)
+        img_CT, img_PT = PatchExtraction.stackImages(addr_patient, ind_CT, ind_PT)
+        patches_CT, patches_PT = PatchExtraction.patch_extraction_thres(img_CT, img_PT, 0)
 
-    ax[0].legend(loc='best')
-    ax[0].set_xticks([])
-    ax[0].set_yticks([])
-    ax[1].legend(loc='best')
-    ax[1].set_xticks([])
-    ax[1].set_yticks([])
+        # Extract Features using initial network
+        print(f"Extract Features using initial network...")
+        features_init = initial_model.predict([patches_CT, patches_PT], steps=1)
 
-    fig.suptitle('Distribution of patches')
-    plt.savefig(f"{path_result}Plot_{path_patient}.png", format='png', dpi=300)
+        # Extract Features using trained network
+        print(f"Extract Features...")
+        features = trained_model.predict([patches_CT, patches_PT], steps=1)
 
-time_end = time.time()
-print(f"Evaluation Finished! Elapsed time: {time_end - time_start}")
+        # Using K-means
+        print(f"K-means Clustering...")
+        num_labels = 5
+        model_k_means = KMeans(n_clusters=num_labels, random_state=0)
+        model_k_means.fit(features)
+        label_predict = model_k_means.fit_predict(features)
+
+        # Dimension reduction using PCA
+        pca = PCA(n_components=2)
+        features_low = pca.fit_transform(features)
+        features_init_low = pca.transform(features_init)
+
+        colors = ['salmon', 'orange', 'steelblue', 'violet', 'khaki']
+        fig, ax = plt.subplots(2, figsize=(5, 5), constrained_layout=True)
+
+        for i in range(5):
+            data_init = features_init_low[label_predict == i]
+            X_init = data_init[:, 0]
+            Y_init = data_init[:, 1]
+            ax[0].scatter(X_init, Y_init, color=colors[i], label=i, s=1)
+
+            data = features_low[label_predict == i]
+            X = data[:, 0]
+            Y = data[:, 1]
+            ax[1].scatter(X, Y, color=colors[i], label=i, s=1)
+
+        ax[0].legend(loc='best')
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        ax[1].legend(loc='best')
+        ax[1].set_xticks([])
+        ax[1].set_yticks([])
+
+        fig.suptitle('Distribution of patches')
+        plt.savefig(f"{path_result}Plot_{path_patient}.png", format='png', dpi=300)
+
+    time_end = time.time()
+    print(f"Evaluation Finished! Elapsed time: {time_end - time_start}")
--- Analysis/SampleVisualization_AE.py
+++ Analysis/SampleVisualization_AE.py
@@ -1,15 +1,16 @@
-# Sample Visualization
-# 1-1. Extract Features using initial network
-# 1-2. Extract Features using trained network
-# 2. Using K-means to classify the patches
-# 3. Dimension reduction using PCA
-# 4. Visualize results
+"""
+    Sample Visualization
+    Make 2-D image of sample distribution
+    1-1. Extract Features using initial network
+    1-2. Extract Features using trained network
+    2. Using K-means to classify the patches
+    3. Dimension reduction using PCA
+    4. Visualize results
+"""
 
 import tensorflow as tf
 import numpy as np
-from tensorflow.python.keras.engine.training import Model
-
-import NetworkKeras
+from Network import NetworkKeras
 import os
 import time
 from Extraction import PatchExtraction
@@ -18,109 +19,121 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from datetime import datetime
 
-tf.enable_eager_execution()
 
-time_start = time.time()
-path_model = './model/AE/'
+def SampleVisualization_AE(path_model, path_image):
+    """ Visualize sample distribution using PCA.
+        The result image will be saved on 'Results_%Y%m%d_%H%M%S'
 
-# Extract Features using trained network
-# Load model
-input_shape = (17 * 17)
+    Input
+    ______
+    path_model: path of trained model
+    path_image: path of test image
 
-initial_model_CT = NetworkKeras.create_autoencoder(input_shape)
-initial_model_PT = NetworkKeras.create_autoencoder(input_shape)
+    Output
+    ______
+    """
 
-trained_model_CT = NetworkKeras.create_autoencoder(input_shape)
-trained_model_CT.load_weights(path_model + 'CT')
+    tf.enable_eager_execution()
 
-trained_model_PT = NetworkKeras.create_autoencoder(input_shape)
-trained_model_PT.load_weights(path_model + 'PT')
+    time_start = time.time()
 
-# Make feature extraction model
-initial_extractor_CT = tf.keras.models.Model(inputs=initial_model_CT.input,
-                                             outputs=initial_model_CT.get_layer('tf_op_layer_l2_normalize').output)
-initial_extractor_PT = tf.keras.models.Model(inputs=initial_model_PT.input,
-                                             outputs=initial_model_PT.get_layer('tf_op_layer_l2_normalize_2').output)
+    # Extract Features using trained network
+    # Load model
+    input_shape = (17 * 17)
 
-feature_extractor_CT = tf.keras.models.Model(inputs=trained_model_CT.input,
-                                             outputs=trained_model_CT.get_layer('tf_op_layer_l2_normalize_4').output)
-feature_extractor_PT = tf.keras.models.Model(inputs=trained_model_PT.input,
-                                             outputs=trained_model_PT.get_layer('tf_op_layer_l2_normalize_6').output)
+    initial_model_CT = NetworkKeras.create_autoencoder(input_shape)
+    initial_model_PT = NetworkKeras.create_autoencoder(input_shape)
 
-# Load Images
-ind_CT = [[230, 380], [150, 370]]
-ind_PT = [[230, 380], [150, 370]]
-path = '../Test'
+    trained_model_CT = NetworkKeras.create_autoencoder(input_shape)
+    trained_model_CT.load_weights(path_model + 'CT')
 
-# Make Results Folder
-now = datetime.now()
-path_result = f"./Results_{now.strftime('%Y%m%d_%H%M%S')}/"
-os.makedirs(path_result)
+    trained_model_PT = NetworkKeras.create_autoencoder(input_shape)
+    trained_model_PT.load_weights(path_model + 'PT')
 
-# Print Patients Number
-patient_dir = os.listdir(path)
-print(f'Patients Number: {len(patient_dir)}')
+    # Make feature extraction model
+    initial_extractor_CT = tf.keras.models.Model(inputs=initial_model_CT.input,
+                                                 outputs=initial_model_CT.get_layer('tf_op_layer_l2_normalize').output)
+    initial_extractor_PT = tf.keras.models.Model(inputs=initial_model_PT.input,
+                                                 outputs=initial_model_PT.get_layer('tf_op_layer_l2_normalize_2').output)
 
-for path_patient in patient_dir:
-    addr_patient = f'{path}/{path_patient}/'\
+    feature_extractor_CT = tf.keras.models.Model(inputs=trained_model_CT.input,
+                                                 outputs=trained_model_CT.get_layer('tf_op_layer_l2_normalize_4').output)
+    feature_extractor_PT = tf.keras.models.Model(inputs=trained_model_PT.input,
+                                                 outputs=trained_model_PT.get_layer('tf_op_layer_l2_normalize_6').output)
 
-    img_CT, img_PT = PatchExtraction.stackImages(addr_patient, ind_CT, ind_PT)
-    patches_CT, patches_PT = PatchExtraction.patch_extraction_thres(img_CT, img_PT, 0)
+    # Load Images
+    ind_CT = [[230, 380], [150, 370]]
+    ind_PT = [[230, 380], [150, 370]]
 
-    # Extract Features using initial network
-    print(f"Extract Features using initial network...")
-    features_init_CT = initial_extractor_CT.predict(patches_CT, steps=1)
-    features_init_PT = initial_extractor_PT.predict(patches_PT, steps=1)
-    features_init = np.hstack((features_init_CT, features_init_PT))
+    # Make Results Folder
+    now = datetime.now()
+    path_result = f"./Results_{now.strftime('%Y%m%d_%H%M%S')}/"
+    os.makedirs(path_result)
 
-    # Extract Features
-    print(f"Extract Features...")
-    features_CT = feature_extractor_CT.predict(patches_CT, steps=1)
-    features_PT = feature_extractor_PT.predict(patches_PT, steps=1)
-    features = np.hstack((features_CT, features_PT))
+    # Print Patients Number
+    patient_dir = os.listdir(path_image)
+    print(f'Patients Number: {len(patient_dir)}')
 
-    # Using K-means
-    print(f"K-means Clustering...")
-    num_labels = 5
-    model_k_means = KMeans(n_clusters=num_labels, random_state=0)
-    model_k_means.fit(features)
+    for path_patient in patient_dir:
+        addr_patient = f'{path_image}/{path_patient}/'\
 
-    # Merging Patches
-    num_x = 44
-    num_y = 30
-    stride = 5
+        img_CT, img_PT = PatchExtraction.stackImages(addr_patient, ind_CT, ind_PT)
+        patches_CT, patches_PT = PatchExtraction.patch_extraction_thres(img_CT, img_PT, 0)
 
-    label_predict = model_k_means.fit_predict(features)
-    label_predict_batch = label_predict.reshape((-1, num_y * num_x))
+        # Extract Features using initial network
+        print(f"Extract Features using initial network...")
+        features_init_CT = initial_extractor_CT.predict(patches_CT, steps=1)
+        features_init_PT = initial_extractor_PT.predict(patches_PT, steps=1)
+        features_init = np.hstack((features_init_CT, features_init_PT))
 
-    # Dimension reduction using PCA
-    pca = PCA(n_components=2)
-    features_low = pca.fit_transform(features)
-    features_init_low = pca.transform(features_init)
+        # Extract Features
+        print(f"Extract Features...")
+        features_CT = feature_extractor_CT.predict(patches_CT, steps=1)
+        features_PT = feature_extractor_PT.predict(patches_PT, steps=1)
+        features = np.hstack((features_CT, features_PT))
 
-    colors = ['salmon', 'orange', 'steelblue', 'violet', 'khaki']
-    fig, ax = plt.subplots(2, figsize=(5, 5), constrained_layout=True)
+        # Using K-means
+        print(f"K-means Clustering...")
+        num_labels = 5
+        model_k_means = KMeans(n_clusters=num_labels, random_state=0)
+        model_k_means.fit(features)
 
-    for i in range(5):
-        data_init = features_init_low[label_predict == i]
-        X_init = data_init[:, 0]
-        Y_init = data_init[:, 1]
-        ax[0].scatter(X_init, Y_init, color=colors[i], label=i, s=1)
+        # Merging Patches
+        num_x = 44
+        num_y = 30
+        stride = 5
 
-        data = features_low[label_predict == i]
-        X = data[:, 0]
-        Y = data[:, 1]
-        ax[1].scatter(X, Y, color=colors[i], label=i, s=1)
+        label_predict = model_k_means.fit_predict(features)
+        label_predict_batch = label_predict.reshape((-1, num_y * num_x))
 
-    ax[0].legend(loc='best')
-    ax[0].set_xticks([])
-    ax[0].set_yticks([])
-    ax[1].legend(loc='best')
-    ax[1].set_xticks([])
-    ax[1].set_yticks([])
+        # Dimension reduction using PCA
+        pca = PCA(n_components=2)
+        features_low = pca.fit_transform(features)
+        features_init_low = pca.transform(features_init)
 
-    fig.suptitle('Distribution of patches')
-    plt.savefig(f"{path_result}Plot_{path_patient}.png", format='png', dpi=300)
+        colors = ['salmon', 'orange', 'steelblue', 'violet', 'khaki']
+        fig, ax = plt.subplots(2, figsize=(5, 5), constrained_layout=True)
 
-time_end = time.time()
-print(f"Evaluation Finished! Elapsed time: {time_end - time_start}")
+        for i in range(5):
+            data_init = features_init_low[label_predict == i]
+            X_init = data_init[:, 0]
+            Y_init = data_init[:, 1]
+            ax[0].scatter(X_init, Y_init, color=colors[i], label=i, s=1)
+
+            data = features_low[label_predict == i]
+            X = data[:, 0]
+            Y = data[:, 1]
+            ax[1].scatter(X, Y, color=colors[i], label=i, s=1)
+
+        ax[0].legend(loc='best')
+        ax[0].set_xticks([])
+        ax[0].set_yticks([])
+        ax[1].legend(loc='best')
+        ax[1].set_xticks([])
+        ax[1].set_yticks([])
+
+        fig.suptitle('Distribution of patches')
+        plt.savefig(f"{path_result}Plot_{path_patient}.png", format='png', dpi=300)
+
+    time_end = time.time()
+    print(f"Evaluation Finished! Elapsed time: {time_end - time_start}")
